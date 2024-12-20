b1363402d7ed6c164bfb1a16459dbf36886ea049
jdhamija816 <jdhamija816@gmail.com>
2024-12-20 12:22:50 +0530

    logout-api

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { register } from '../controllers/register.js'
 import { login } from '../controllers/login.js'
 import { check_login } from '../controllers/check_login.js'
+import { logout } from '../controllers/logout.js'
 
 const userRouter = Router()
 
 userRouter.post('/register', register)
 userRouter.post('/login', login)
 userRouter.get('/check_login', check_login)
+userRouter.post('/logout', logout)
 
 export default userRouter
