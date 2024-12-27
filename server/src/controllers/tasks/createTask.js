7757abbdbc85eeac8b735cf3054c64d56e97dd7b
jdhamija816 <jdhamija816@gmail.com>
2024-12-27 13:30:01 +0530

    bug fix

--- server/src/controllers/tasks/createTask.js
+++ server/src/controllers/tasks/createTask.js
@@ -7,18 +7,15 @@ export const createTask = async (req, res) => {
         const authToken = req.cookies.accessToken;
 
         if (!authToken) {
-            // If no token is found, user is not logged in
             return res.status(200).json({
                 message: 'no token is found',
                 isLoggedIn: false,
             });
         }
 
-        // Verify the token
         const userData = verifyToken(authToken);
 
         if (!userData) {
-            // If token is invalid, user is not logged in
             return res.status(200).json({
                 message: 'token is Invalid',
                 isLoggedIn: false
